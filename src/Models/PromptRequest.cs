dab8d03a89fc60bd2fdb2fdf69998d311b2acf11
Kristoffer Dahl <kridah@gmail.com>
2025-05-14 20:09:39 +0200

    Remove PromptRequest class. Was not used

--- src/Models/PromptRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace src.Models;
-
-public class PromptRequest
-{
-    public string Prompt { get; set; } = string.Empty;
-    public string Message { get; set; } = string.Empty;
-    public string? Model { get; set; }
-}
